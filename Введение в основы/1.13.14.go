ec9ff5cdbd9e14ddd6f25eb9b4db00c6f1a72bf6
chevychelovN <86499004+chevychelovN@users.noreply.github.com>
2022-09-10 18:30:52 +0300

    Added task from 1st part of Go course

---
+++ Введение в основы/1.13.14.go
@@ -0,0 +1,11 @@
+// Дано натуральное число N. Выведите его представление в двоичном виде.
+
+package main
+
+import "fmt"
+
+func main() {
+    var a int
+    fmt.Scan(&a)
+        fmt.Printf("%b", a)
+}
